80d02cf093273d3c10c2d8e3466c0ff8a371c028
MariaPinto <maria.fullstack@gmail.com>
2021-03-29 23:10:33 +0200

    typeof

--- JS/05-tipo-de-datos.js
+++ JS/05-tipo-de-datos.js
@@ -7,3 +7,21 @@ var operacion = numero1 + numero2;
 
 
 alert("El resultado de la operación es: "+operacion);
+
+
+// Tipo de datos
+var numero_entero = 44;  // Tipo number
+var cadena_texto = "Hola que tal, como estas?"  // Tipo String
+var verdadero_o_falso = false;
+var numero_falso = "33";
+
+
+
+
+
+console.log(typeof numero_entero) // Typeof nos da el tipo de dato que contiene; (tipo number)
+console.log(typeof cadena_texto) // Tipo string
+console.log(typeof verdadero_o_falso) // Tipo booleano
+console.log(typeof numero_falso)  // Tipo string
+
+
