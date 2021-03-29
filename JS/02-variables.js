ea5a3d741119fa6b5ac77e4735162ba38f214f2f
MariaPinto <maria.fullstack@gmail.com>
2021-03-29 22:05:00 +0200

    var

--- JS/02-variables.js
+++ JS/02-variables.js
@@ -1,12 +1,17 @@
 /* Variables :
-- Una variable es un contenedor de información
+- Una variable es un contenedor de información.
+- Las variables deben definirse bien, para no crear confusiones. 
 */ 
 'use strict';
 
 var pais = "España";
 var continente = "Europa";
 var antiguedad = 2021;
+var pais_y_continente = pais+ ' '+continente; // Concatenados
 
+pais = "Mexico"; // Cambiar el contenido a la variable
+continente = "LatinoAmerica"
 
 console.log(pais, continente, antiguedad);
+alert(pais_y_continente)
 
