919e271d048c647cf0e7701e27335f91bc995cff
MariaPinto <maria.fullstack@gmail.com>
2021-03-29 22:48:53 +0200

    constantes

---
+++ JS/04-constantes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Constantes:
+- Como una varibale, pero su valor no puede cambiar;
+*/ 
+
+var web = "https://mariapinto.es";
+
+const ip = "192.88.0.12";
+
+
+web = "https://mariapinto.es";
+console.log(web, ip);
