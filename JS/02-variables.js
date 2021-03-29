962de978c84360a73bcc0b50a8e4a3e8e6e957ee
MariaPinto <maria.fullstack@gmail.com>
2021-03-29 21:22:15 +0200

    creando las primeras variables

---
+++ JS/02-variables.js
@@ -0,0 +1,12 @@
+/* Variables :
+- Una variable es un contenedor de información
+*/ 
+'use strict';
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2021;
+
+
+console.log(pais, continente, antiguedad);
+
