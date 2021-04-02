3e21e2f91e48cf8b7a46e112b967083ae368a48e
MariaPinto <maria.fullstack@gmail.com>
2021-04-02 21:58:05 +0200

    creando bucle for

--- JS/08-bucle-For.js
+++ JS/08-bucle-For.js
@@ -1,3 +1,8 @@
 'use strict';
 // Bucle for
-// Un bucle es una estructura de control, 
+// Un bucle es una estructura de control que se repite varias veces ;
+
+var numero = 100;
+
+for(i = 0; i < numero; i++);
+
