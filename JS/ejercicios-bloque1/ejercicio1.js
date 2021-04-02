873dd45070fc16a3754fa84c775e4a9e2c49db3e
MariaPinto <maria.fullstack@gmail.com>
2021-04-02 22:50:01 +0200

    creando carpeta de ejercicios bloque 1

---
+++ JS/ejercicios-bloque1/ejercicio1.js
@@ -0,0 +1,5 @@
+'use strict';
+/* Hacer un programa que pida dos numeros y que nos diga cual es el mayor y cual es el menor o si son iguales */ 
+
+var numero1 = parseInt(prompt("Introduce el primer número", 0));
+var numero2 = parseInt(prompt("Introduce el segundo número", 0));
