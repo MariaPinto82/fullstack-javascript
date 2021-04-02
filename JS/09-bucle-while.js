291fc4762eb41f7ed59e6cf56261a3cfa21e7aa8
MariaPinto <maria.fullstack@gmail.com>
2021-04-02 22:25:50 +0200

    aprendiendo a usar break

--- JS/09-bucle-while.js
+++ JS/09-bucle-while.js
@@ -6,10 +6,21 @@ var año = 2021;
 while(año <= 2051){
     // Ejecuta esto
     console.log(año);
-    año++;   // Cuando llegue a 2051 el bucle se detendra
+
+
+    if(año == 2000){
+        break;
+    }
+    año--;   // Cuando llegue a 2000 el bucle se detendra
 }
 
-// Tambien podemos restar en vez de sumar; e ir quitando años;
 
 
 // Bucle Do While;
+
+var year = 30;
+
+do{
+    alert("Solo cuando sea diferente a 18")
+    year--;
+}while(year > 25);
