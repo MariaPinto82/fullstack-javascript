f8335e4ff291db3c1a2b13c08a37bbfadf5b331a
MariaPinto <maria.fullstack@gmail.com>
2021-04-02 23:02:47 +0200

    primer ejercicio acabado

--- JS/ejercicios-bloque1/ejercicio1.js
+++ JS/ejercicios-bloque1/ejercicio1.js
@@ -3,3 +3,18 @@
 
 var numero1 = parseInt(prompt("Introduce el primer número", 0));
 var numero2 = parseInt(prompt("Introduce el segundo número", 0));
+
+console.log(numero1, numero2);
+
+if(numero1 === numero2){
+    alert("Los números son iguales")
+}else if(numero1 > numero2){
+    alert("El nùmero mayor es:" + numero1);
+    alert("El nùmero menor es:" + numero2)
+}else if(numero2 > numero1){
+    alert("El nùmero mayor es:" + numero2);
+    alert("El nùmero menor es:" + numero1);
+}else{
+    alert("Introduce números correctos");
+}
+
