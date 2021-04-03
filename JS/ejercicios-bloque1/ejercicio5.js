b722688e51f41b4f80c136475777fed1746a32ea
MariaPinto <maria.fullstack@gmail.com>
2021-04-04 00:04:15 +0200

    ejercicios 5 y 6

--- JS/ejercicios-bloque1/ejercicio5.js
+++ JS/ejercicios-bloque1/ejercicio5.js
@@ -3,6 +3,9 @@
 /* Muestre todos los numeros divisores de un numero introducido en un prompt */
 let numero = parseInt(prompt("Mete un numero", 0));
 
-for(let i = 1; i <= numero; i++){
-    console.log("Divisor: "+ i);
+for (let i = 1; i <= numero; i++) {
+
+    if (numero%i == 0) {
+        console.log("Divisor: " + i);
+    }
 }
