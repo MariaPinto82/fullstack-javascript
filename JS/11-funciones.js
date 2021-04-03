d4b860a01c662a7a5bdc4214e8a723a47e88b6c0
MariaPinto <maria.fullstack@gmail.com>
2021-04-04 01:26:13 +0200

    definicion de funcion

---
+++ JS/11-funciones.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Funciones:
+ * Las funciones son uno de los bloques de contruccion fundamentales en JavaScript.
+ * Una funcion en JavaScript es similar a un procedimiento--Un conjunto de instrucciones
+ * que realiza una tarea o calcula un valor, pero para que un procedimiento califique como
+ * funcion, debe tomar alguna entrada y devolver una salida donde hay alguna relacion obvia
+ * entre la entrada y la salida. Para usar una funcion, debes definirla en algun lugar del
+ * ambito del que deseas llamarla.
+ */
+
+/**
+ * Una funcion es una agrupacion reutilizable de un conjunto de instrucciones, la cual puedes invocar
+ * las veces que quieras;
+ */
+
